ce72f43b4deca141e596b6c33643618d4f5d5596
Batheesha222 <batheesha.dilmeen@gmail.com>
2025-01-01 21:52:20 +0530

    add product model and update package.json for MongoDB dependency

---
+++ backend/models/productModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import productSchema from './productModel';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true
+    }
+});
+
+const productModel = mongoose.models.product || mongoose.model('product', productSchema);
+
+export default productModel;
